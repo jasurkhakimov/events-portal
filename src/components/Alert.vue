e00b193a3c9178bf7341282c9718968aee4cbe3c
Jasur Khakimov <jasur.khakimov9@gmail.com>
2022-01-10 17:42:26 +0500

    utils/rules added, alert functionality added

---
+++ src/components/Alert.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <v-snackbar
+            :color="alert.color"
+            top
+            right
+            v-model="alert.status"
+            :timeout="timeout"
+        >
+            {{ alert.text }}
+            <template v-slot:action="{ attrs }">
+                <v-btn icon v-bind="attrs" @click="resetAlert()">
+                    <v-icon> mdi-close </v-icon>
+                </v-btn>
+            </template>
+        </v-snackbar>
+    </div>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+import { RESET_ALERT } from "@/store/modules/alert/types";
+import { Module } from "@/store/modules";
+
+export default {
+    data: () => ({
+        snackbar: true,
+        timeout: 1500,
+    }),
+    computed: {
+        ...mapState({
+            alert: (state) => state.alert.alert,
+        }),
+    },
+    methods: {
+        ...mapActions(Module.ALERT, {
+            resetAlert: RESET_ALERT,
+        }),
+    },
+};
+</script>
+
+<style>
+</style>
---
+++ src/store/modules/alert/index.js
@@ -0,0 +1,20 @@
+import mutations from './mutations';
+import actions from './actions';
+import getters from './getters';
+
+
+const state = {
+    alert: {
+        status: false,
+        text: '',
+        color: ''
+    }
+}
+
+export default {
+    namespaced: true,
+    state,
+    mutations,
+    actions,
+    getters 
+};
